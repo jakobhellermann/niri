f375de9f336b4e28785b03a82c33727aa6833df1
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-04-28 12:01:46 +0200

    window-rule: Add disable-mod-mouse-actions for Mod+Mouse opt-out

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -887,6 +887,7 @@ mod tests {
                 open-fullscreen false
                 open-floating false
                 open-focused true
+                disable-mod-mouse-actions true
                 default-window-height { fixed 500; }
                 default-column-display "tabbed"
                 default-floating-position x=100 y=-200 relative-to="bottom-left"
@@ -1874,6 +1875,9 @@ mod tests {
                     ),
                     scroll_factor: None,
                     tiled_state: None,
+                    disable_mod_mouse_actions: Some(
+                        true,
+                    ),
                     background_effect: BackgroundEffectRule {
                         xray: None,
                         blur: None,
